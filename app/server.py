358593e50d914f3fb37812b786fd684d91b4c12b
ikswolachim <40395525+ikswolachim@users.noreply.github.com>
2019-12-27 01:24:51 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1Kkw9Qy7OXLfDXS5UkUlZsDbxd3LqwV4q'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['fat_bike','dh_bike','road_bike','dj_bike','bmx_bike','enduro_bike']
 path = Path(__file__).parent
 
 app = Starlette()
